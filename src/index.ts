e118060d5e8fd0d8d357645b650e2a6497a79394
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-01-09 11:49:41 -0300

    added exercise 1 - configuring the server node with express so that it starts with the start script.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,21 @@ import { users, products,purchase,
 createUser, getAllUsers, createProduct, getAllProducts, getProductById, 
 queryProductsByName, createPurchase, getAllPurchasesFromUserId } from "./database"
 import {PRODUCT_CATEGORY} from "./types"
+import express, { Request, Response } from 'express'
+import cors from 'cors'
 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
 
 //createUser("u003", "beltrano@email.com", "beltrano99")
 //console.table(getAllUsers())
@@ -11,7 +25,7 @@ import {PRODUCT_CATEGORY} from "./types"
 //console.table(getProductById("prod1"))
 //console.log(queryProductsByName("HEADSET"))
 //createPurchase("u003", "p004", 2, 1600)
-console.table(getAllPurchasesFromUserId("user1"))
+//console.table(getAllPurchasesFromUserId("user1"))
 
 
 
